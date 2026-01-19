dc9b4b27d6c1a9ea84b0be3a38911d74a0ed3694
valan23 <carlosgmoya@gmail.com>
2026-01-19 13:57:38 +0100

    Icono estrella Edicion main.js

--- main.js
+++ main.js
@@ -85,18 +85,23 @@ function renderGames(games) {
 
             <span class="game-title">${j["Nombre Juego"]}</span>
 
-            ${isValid(j["Edición"]) ? `
+            ${isValid(j["Edición"]) && j["Edición"].toUpperCase() !== "ESTÁNDAR" && j["Edición"].toUpperCase() !== "ESTANDAR" ? `
                 <div class="edition-text" style="
                     font-family: 'Segoe UI', Roboto, sans-serif;
                     font-size: 0.75em; 
                     font-style: italic; 
                     color: #aaa; 
                     margin-top: -4px; 
-                    margin-bottom: 15px; /* Aumentado de 5px a 15px para dar más aire */
-                    line-height: 1.2;">
+                    margin-bottom: 15px; 
+                    line-height: 1.2;
+                    display: flex;
+                    align-items: center;
+                    gap: 4px;">
+                    <i class="fa-solid fa-star" style="color: #ffd700; font-size: 0.9em;"></i>
                     ${j["Edición"]}
                 </div>` : `
-                <div style="margin-bottom: 15px;"></div> `}
+                <div style="margin-bottom: 15px;"></div>
+            `}
 
             <div class="details-grid" style="font-family: 'Segoe UI', Roboto, sans-serif; font-size: 0.8em; letter-spacing: 0.3px; line-height: 1.4; margin-top: 10px;">
                 ${isValid(j["Estado Caja"]) ? `<div><span style="color: #aaa;">📦Caja:</span> ${formatEstado(j["Estado Caja"])}</div>` : ''}
