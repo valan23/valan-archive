1096f02c8e1a35b7ece9fc6d3e820370a89e8ab8
valan23 <carlosgmoya@gmail.com>
2026-01-29 10:16:36 +0100

    Update games.js

--- js/games.js
+++ js/games.js
@@ -90,7 +90,7 @@ function createCardHTML(j) {
                 </div>
             </div>
 
-            <div style="margin-bottom: 12px; padding: 5px 0 5px 12px; border-left: 3px solid ${esDigital ? '#00d4ff' : 'rgba(255,255,255,0.1)'}; margin-right: 12px;">
+            <div style="margin-bottom: 12px; padding: 5px 0; margin-right: 12px;">
                 <div class="game-title" style="font-size: 1.1em; color: #EFC36C; font-weight: 700; line-height: 1.2;">
                     ${j["Nombre Juego"]}
                 </div>
